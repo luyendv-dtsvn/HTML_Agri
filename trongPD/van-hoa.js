7d948d032e53d0a5df246e087c6b058f8fc60304
YOUsomuchlove <ductrongbk2001vn@gmail.com>
2024-11-26 17:41:05 +0700

    page văn hóa Agribank

---
+++ trongPD/van-hoa.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".agribank-culture-identity-card");
+
+    cards.forEach((card) => {
+        card.addEventListener("mouseenter", () => {
+            cards.forEach((c) => c.classList.remove("active"));
+            card.classList.add("active");
+        });
+
+        card.addEventListener("mouseleave", () => {
+            card.classList.remove("active");
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".featured-culture-item");
+
+    items.forEach((item) => {
+        item.addEventListener("click", () => {
+            // Loại bỏ trạng thái active khỏi các item khác
+            items.forEach((el) => el.classList.remove("active"));
+            // Thêm trạng thái active vào item hiện tại
+            item.classList.toggle("active");
+        });
+    });
+});
